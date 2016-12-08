c033cf2dec1ca320cfbbf07a27e9a5fae698a2b0
Thomas <Thomas.Face03@gmail.com>
2016-12-08 11:39:05 +0100

    JavaScript

---
+++ JavaScript.js
@@ -0,0 +1,4 @@
+badgesHTML = prompt ('Combien de badges HTML as tu?'); //Question pour l'utilisateur//
+badgesCSS = prompt ('Combien de badges CSS as tu?');
+totalbadges = parseInt(badgesHTML) + parseInt(badgesCSS);
+alert('Woaw, tu as '+totalbadges+' badges !'); //Alerte pour afficher le total//
